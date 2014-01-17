d36fdaafe393fddab367a266941316f910980385
chrislindsay <chris.lindsay@jlta.com.au>
2014-01-17 14:19:11 +0800

    Test Adding files

---
+++ TestApp/JLTTestClass.h
@@ -0,0 +1,13 @@
+//
+//  JLTTestClass.h
+//  TestApp
+//
+//  Created by Chris Lindsay on 17/01/2014.
+//  Copyright (c) 2014 Chris Lindsay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JLTTestClass : NSObject
+
+@end
---
+++ TestApp/JLTTestClass.m
@@ -0,0 +1,13 @@
+//
+//  JLTTestClass.m
+//  TestApp
+//
+//  Created by Chris Lindsay on 17/01/2014.
+//  Copyright (c) 2014 Chris Lindsay. All rights reserved.
+//
+
+#import "JLTTestClass.h"
+
+@implementation JLTTestClass
+
+@end
